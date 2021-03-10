de3afb1fef6f0cb647c3d7c88f00ae60a69dd56f
Andrey <andrii.yevsega@gmail.com>
2021-03-10 14:26:12 +0200

    test

--- src/containers/Profile/Profile.tsx
+++ src/containers/Profile/Profile.tsx
@@ -1,6 +1,5 @@
 import React, { FC } from "react";
 import { Flex } from "@chakra-ui/react";
-
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Content from "./components/Content";
